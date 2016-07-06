15f2730cebbd06301f9968ebc2bf57c50f7b0ee8
cathulhu <cathulhu@users.noreply.github.com>
2016-07-06 02:30:28 -0700

    linked UI creation of background object and added corresponding background object features.

---
+++ app/src/main/java/com/prototype/balcorasystems/slw3/Object_Background.java
@@ -0,0 +1,151 @@
+package com.prototype.balcorasystems.slw3;
+
+
+import android.util.Log;
+
+public class Object_Background {
+    private float annualRaise;
+    private float[] annualSalaries;   //maybe ill make this array list, have to think about that
+    private String owner;
+    private String employmentSector;
+    private int earliestActiveLoanDate;
+    private boolean employedFulltime;
+    private boolean publicServiceForgivenessEligible;
+    private boolean collectingSsdi;
+    private boolean totalDisability;
+    private boolean teacherAtLowIncomeSchool;
+    private boolean militaryDisability;
+    private boolean deceasedChildParentLoan;
+
+    public Object_Background() {
+
+    }
+
+    public Object_Background(float raise, String industry, boolean fulltime, boolean ssdi, boolean totallyDisabled, boolean lowIncomeSchoolTeacher, boolean vaDisability, boolean childDeceasedLoan) {
+        annualRaise=raise;
+        employmentSector=industry;
+        employedFulltime=fulltime;
+        collectingSsdi=ssdi;
+        totalDisability=totallyDisabled;
+        teacherAtLowIncomeSchool=lowIncomeSchoolTeacher;
+        militaryDisability=vaDisability;
+        deceasedChildParentLoan=childDeceasedLoan;
+
+        float fakeTestincome = 10000.00f;
+
+        annualSalaries=this.calculateFutureSalaries(fakeTestincome, annualRaise);
+    }
+
+    public float[] calculateFutureSalaries (float income, float raise) {
+
+        float[] results = new float[25];
+        results[0]=income;
+        float raisedSalary = income;
+
+        for (int i=1; i < 25; i++)
+        {
+            raisedSalary += raisedSalary*raise;
+            results[i]=raisedSalary;
+
+        }
+
+        return results;
+    }
+
+
+    public float getAnnualRaise() {
+        return annualRaise;
+    }
+
+    public void setAnnualRaise(int annualRaise) {
+        this.annualRaise = annualRaise;
+    }
+
+    public float[] getAnnualSalaries() {
+        return annualSalaries;
+    }
+
+    public void setAnnualSalaries(float[] annualSalaries) {
+        this.annualSalaries = annualSalaries;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getEmploymentSector() {
+        return employmentSector;
+    }
+
+    public void setEmploymentSector(String employmentSector) {
+        this.employmentSector = employmentSector;
+    }
+
+    public int getEarliestActiveLoanDate() {
+        return earliestActiveLoanDate;
+    }
+
+    public void setEarliestActiveLoanDate(int earliestActiveLoanDate) {
+        this.earliestActiveLoanDate = earliestActiveLoanDate;
+    }
+
+    public boolean isEmployedFulltime() {
+        return employedFulltime;
+    }
+
+    public void setEmployedFulltime(boolean employedFulltime) {
+        this.employedFulltime = employedFulltime;
+    }
+
+    public boolean isPublicServiceForgivenessEligible() {
+        return publicServiceForgivenessEligible;
+    }
+
+    public void setPublicServiceForgivenessEligible(boolean publicServiceForgivenessEligible) {
+        this.publicServiceForgivenessEligible = publicServiceForgivenessEligible;
+    }
+
+    public boolean isCollectingSsdi() {
+        return collectingSsdi;
+    }
+
+    public void setCollectingSsdi(boolean collectingSsdi) {
+        this.collectingSsdi = collectingSsdi;
+    }
+
+    public boolean isTotalDisability() {
+        return totalDisability;
+    }
+
+    public void setTotalDisability(boolean totalDisability) {
+        this.totalDisability = totalDisability;
+    }
+
+    public boolean isTeacherAtLowIncomeSchool() {
+        return teacherAtLowIncomeSchool;
+    }
+
+    public void setTeacherAtLowIncomeSchool(boolean teacherAtLowIncomeSchool) {
+        this.teacherAtLowIncomeSchool = teacherAtLowIncomeSchool;
+    }
+
+    public boolean isMilitaryDisability() {
+        return militaryDisability;
+    }
+
+    public void setMilitaryDisability(boolean militaryDisability) {
+        this.militaryDisability = militaryDisability;
+    }
+
+    public boolean isDeceasedChildParentLoan() {
+        return deceasedChildParentLoan;
+    }
+
+    public void setDeceasedChildParentLoan(boolean deceasedChildParentLoan) {
+        this.deceasedChildParentLoan = deceasedChildParentLoan;
+    }
+}
--- app/src/main/java/com/prototype/balcorasystems/slw3/UI_Fragment_Background.java
+++ app/src/main/java/com/prototype/balcorasystems/slw3/UI_Fragment_Background.java
@@ -1,48 +1,184 @@
 package com.prototype.balcorasystems.slw3;
 
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 
 public class UI_Fragment_Background extends Fragment{
+
+    float annualRaise;
+    String industry;
+    boolean fulltime;
+    boolean ssdi;
+    boolean totallydisabled;
+    boolean lowIncomeTeacher;
+    boolean millitarydisability;
+    boolean deceasedChildParentLoan;
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+
+        Object_Background newBackground = new Object_Background(annualRaise, industry, fulltime, ssdi, totallydisabled, lowIncomeTeacher, millitarydisability, deceasedChildParentLoan);
+    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.background, container, false);
 
+        final Spinner raiseSpinner = (Spinner) view.findViewById(R.id.annualRaiseSpinner);
+        final Spinner jobSpinner = (Spinner) view.findViewById(R.id.industrySpinner);
 
-        final Spinner raiseSpinner = (Spinner) view.findViewById(R.id.spinner2);
-// Create an ArrayAdapter using the string array and a default spinner layout
+        final RadioGroup fulltimeGroup = (RadioGroup) view.findViewById(R.id.fulltimeRadioGroup);
+        final RadioGroup ssdiGroup = (RadioGroup) view.findViewById(R.id.ssdiRadioGroup);
+        final RadioGroup totalDisabilityGroup = (RadioGroup) view.findViewById(R.id.totalDisabilityRadioGroup);
+        final RadioGroup lowIncomeTeacherGroup = (RadioGroup) view.findViewById(R.id.lowIncomeTeacherRadioGroup);
+        final RadioGroup militaryDisabilityGroup = (RadioGroup) view.findViewById(R.id.millitaryDisabilityRadioGroup);
+        final RadioGroup childDeceasedGroup = (RadioGroup) view.findViewById(R.id.parentPlusDeceasedRadioGroup);
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(getContext(), R.array.annual_raise_array, android.R.layout.simple_spinner_dropdown_item);
-// Specify the layout to use when the list of choices appears
+        // Specify the layout to use when the list of choices appears
         adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-// Apply the adapter to the spinner
+        // Apply the adapter to the spinner
         raiseSpinner.setAdapter(adapter1);
 
-        final Spinner jobSpinner = (Spinner) view.findViewById(R.id.spinner3);
-// Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(getContext(), R.array.jobs_array, android.R.layout.simple_spinner_dropdown_item);
-// Specify the layout to use when the list of choices appears
         adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-// Apply the adapter to the spinner
         jobSpinner.setAdapter(adapter2);
 
 
+        raiseSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
+                annualRaise=position/100.0f;
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        jobSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                industry = jobSpinner.getSelectedItem().toString();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+        fulltimeGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                RadioButton checkedRadioButton = (RadioButton)fulltimeGroup.findViewById(checkedId);
+
+                int choice = fulltimeGroup.indexOfChild(checkedRadioButton);
+
+                if (choice==0)
+                {
+                    fulltime=true;
+                }
+            }
+        });
+
+        ssdiGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                RadioButton checkedRadioButton = (RadioButton)ssdiGroup.findViewById(checkedId);
+
+                int choice = ssdiGroup.indexOfChild(checkedRadioButton);
+
+                if (choice==0)
+                {
+                    ssdi=true;
+                }
+            }
+        });
+
+        totalDisabilityGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                RadioButton checkedRadioButton = (RadioButton)totalDisabilityGroup.findViewById(checkedId);
+
+                int choice = totalDisabilityGroup.indexOfChild(checkedRadioButton);
+
+                if (choice==0)
+                {
+                    totallydisabled=true;
+                }
+            }
+        });
+
+        lowIncomeTeacherGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                RadioButton checkedRadioButton = (RadioButton)lowIncomeTeacherGroup.findViewById(checkedId);
+
+                int choice = lowIncomeTeacherGroup.indexOfChild(checkedRadioButton);
+
+                if (choice==0)
+                {
+                    lowIncomeTeacher=true;
+                }
+            }
+        });
+
+        militaryDisabilityGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                RadioButton checkedRadioButton = (RadioButton)militaryDisabilityGroup.findViewById(checkedId);
+
+                int choice = militaryDisabilityGroup.indexOfChild(checkedRadioButton);
+
+                if (choice==0)
+                {
+                    millitarydisability=true;
+                }
+            }
+        });
+
+        childDeceasedGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                RadioButton checkedRadioButton = (RadioButton)childDeceasedGroup.findViewById(checkedId);
+
+                int choice = childDeceasedGroup.indexOfChild(checkedRadioButton);
+
+                if (choice==0)
+                {
+                    deceasedChildParentLoan=true;
+                }
+            }
+        });
 
         return view;
-    }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
     }
 
 }
